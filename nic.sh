720499e45e387447353ea228683f45482dfb3c3e
Agarrido <Agarrido@gmail.com>
2023-06-20 09:16:31 +0200

    separado

--- nic.sh
+++ nic.sh
@@ -1,3 +1,10 @@
+GRUPO=__grupo__
+loca=__loca__
+vnetname=__vnetname__
+nicname=__nicname__
+subnetname=__subnetname__
+nsgname=__nsgname__
+
 #NIC create
 az network nic create \
     --name $nicname \
